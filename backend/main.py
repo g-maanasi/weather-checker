1ea446166c50a3be95d3369b15e892e348ea4b14
Maanasi Ganapathy <maanasiganapathy@gmail.com>
2025-05-23 17:16:40 -0500

    Added login api route and cookies to sign up

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ from retry_requests import retry
 import json
 import sqlite3
 import bcrypt
+import secrets
 
 # Setup the Open-Meteo API client with cache and retry on error
 cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
@@ -35,9 +36,9 @@ def get_conn():
 def test():
     return RedirectResponse(url="/api")
 
-'''
-' WEATHER FUNCTIONS
-'''
+#####################
+# WEATHER FUNCTIONS #
+#####################
 class WeatherOptions(BaseModel):
     latitude: int = 52.52
     longtitude: int = 13.41
@@ -77,15 +78,19 @@ def get_weather(latitude, longitude, speed_unit, temperature_unit):
     print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
 
-'''
-' SIGN UP FUNCTIONS
-'''
+################################
+# SIGN UP AND LOG IN FUNCTIONS #
+################################
 class SignUpInfo(BaseModel):
     first_name: str = "John"
     last_name: str = "Smith"
     email: str = "example@gmail.com"
     password: str = "abcd"
     role: str = "user"
+
+class LoginInfo(BaseModel):
+    email: str | None
+    password: str | None
 
 @app.post('/api/signup')
 def signup(user_info: SignUpInfo, response: Response):
@@ -103,12 +108,61 @@ def signup(user_info: SignUpInfo, response: Response):
     # Check if email is already in the table
     result = cursor.execute("SELECT * FROM users WHERE email=?;", (user_info.email,))
     if result:
+        conn.close()
         return {'success': False, 'reason': 'email already exists'}
 
     # Insert data into the users table
     query = '''INSERT INTO users (first_name, last_name, email, role, password_hash, salt) VALUES (?,?,?,?,?,?);'''
     cursor.execute(query, (user_info.first_name, user_info.last_name, user_info.email, user_info.role, password_hash, salt))
 
+    # Add user to cookies with time limit
+    response.set_cookie(
+        key="email",
+        value=user_info.email,
+        max_age=86400,
+        httponly=True,
+        secure=True,
+        samesite="lax"
+    )
+
     # Close connection and return success
     conn.close()
     return {'success': True, 'reason':  None}
+
+@app.post('/api/login')
+def login(user_info: SignUpInfo, response: Response):
+    if not user_info or not user_info.email or not user_info.password:
+        return {'success': False, 'reason': 'no user information'}
+    
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    # Check if the email is in the table and encrypt the password to see if it matches
+    query = '''SELECT password_hash, salt FROM users WHERE email=?;'''
+    result = cursor.execute(query, (user_info.email,))
+
+    if not result:
+        conn.close()
+        return {'success': False, 'reason': "Email doesn't exist"}
+    
+    salt = result[0]['salt']
+    password_hash = result[0]['password_hash']
+    current_pass_attempt = user_info.password.encode('utf-8')
+    current_pass_attempt = bcrypt.hashpw(current_pass_attempt, salt)
+
+    if current_pass_attempt == password_hash:
+        # Add user to cookies
+        response.set_cookie(
+            key="email",
+            value=user_info.email,
+            max_age=86400,
+            httponly=True,
+            secure=True,
+            samesite="lax"
+        )
+
+        conn.close()
+        return {'success': True, 'reason': None}
+    
+    conn.close()
+    return {'success': False, 'reason': "password hash did not match"}
